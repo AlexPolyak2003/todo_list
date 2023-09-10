3b324fb9c5343c7d593c98e3597ba57f67b2eab6
AlexPolyak2003 <polyanichko03@inbox.ru>
2023-09-10 14:42:11 +0300

    M

--- src/components/ToDoList/ToDoItem/ToDoItem.tsx
+++ src/components/ToDoList/ToDoItem/ToDoItem.tsx
@@ -18,27 +18,29 @@ export const ToDoItem: React.FC<TodoItemProps> = ({
 }) => {
   console.log(todo);
   return (
-    <div className={styles.todo_item_container}>
-      <div
-        aria-hidden
-        style={{
-          opacity: todo.checked ? 0.5 : 1,
-          textDecoration: todo.checked ? "line-through" : "none",
-        }}
-        onClick={() => checkTodo(todo.id)}
-        className={styles.todo_item_title}
-      >
-        {todo.name}
-      </div>
-      <div className={styles.todo_item_description}>{todo.description}</div>
+    <div className={styles.wrapper}>
+      <div className={styles.todo_item_container}>
+        <div
+          aria-hidden
+          style={{
+            opacity: todo.checked ? 0.5 : 1,
+            textDecoration: todo.checked ? "line-through" : "none",
+          }}
+          onClick={() => checkTodo(todo.id)}
+          className={styles.todo_item_title}
+        >
+          {todo.name}
+        </div>
+        <div className={styles.todo_item_description}>{todo.description}</div>
 
-      <div className={styles.todo_item_button_container}>
-        <Button color="orange" onClick={() => selecTodoIdForEdit(todo.id)}>
-          EDIT
-        </Button>
-        <Button color="red" onClick={() => deleteTodo(todo.id)}>
-          DELETE
-        </Button>
+        <div className={styles.todo_item_button_container}>
+          <Button color="orange" onClick={() => selecTodoIdForEdit(todo.id)}>
+            EDIT
+          </Button>
+          <Button color="red" onClick={() => deleteTodo(todo.id)}>
+            DELETE
+          </Button>
+        </div>
       </div>
     </div>
   );
